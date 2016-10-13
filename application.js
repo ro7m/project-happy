ce5eff1020dcbd0fea7da3e7b53c9f44cfd9cb10
Stanley Nguyen <hung.ngn.the@gmail.com>
2016-10-13 16:53:41 +0800

    add options for prevideo activity

--- application.js
+++ application.js
@@ -1,4 +1,4 @@
-var nextPageToken, currentQuery, playedVideo, playDuration = 120000, directedAction, player;
+var nextPageToken, currentQuery, playedVideo, playDuration = 120000, previdActivity, directedAction, player;
 var $resultDisplay = $('#search-result'),
     $spinner = $('.spinner'),
     $loadMoreButton = $('#load-more-results'),
@@ -25,6 +25,14 @@ $(document).ready(function() {
             $loadMoreButton.css('display', 'block');
         });
     }
+
+    $(document).on( 'change', '#prevideo-activity', function (event) {
+        if ( event.currentTarget.checked ) {
+            $('#prevideo-activity-options').html(prevideoActivityOptions);
+        } else {
+            $('#prevideo-activity-options').html('');
+        }
+    });
 
     $(document).on( 'submit', '#video-search', function(event) {
         event.preventDefault();
@@ -76,22 +84,46 @@ $(document).ready(function() {
     $(document).on('submit', '#timer', function(event) {
         event.preventDefault();
         $timerModal.modal('hide');
+        previdActivity = $(event.currentTarget).find('input[name="prevideo-activity"]:checked').val();
+        var minutesToPrevidActivity = $(event.currentTarget).find('input[name="prevideo-duration"]').val() * 60000 || 120000;
         var minutesToPlay = $(event.currentTarget).find('input[name="time-duration"]').val();
         if (minutesToPlay) playDuration = minutesToPlay * 60000;
         directedAction = $(event.currentTarget).find('input[name="directed-action"]:checked').val();
         $videoScreening.show();
-        player = new YT.Player('video-screening', {
-            height: '390',
-            width: '640',
-            videoId: playedVideo,
-            playerVars: {autoplay: 1, loop: 1, playlist: playedVideo},
-            events: {
-                'onReady': countDown
-            }
-        });
+        switch (previdActivity) {
+            case 'Draw':
+                prevideoCountDown('./img/drawing.jpg', minutesToPrevidActivity);
+                break;
+            case 'Eat Snack':
+                prevideoCountDown('./img/snack-time.jpg', minutesToPrevidActivity);
+                break;
+            default:
+                player();
+                break;
+        }
     });
 
 });
+
+function prevideoCountDown (imgURL, duration) {
+    $videoScreening.html('<img class="img-responsive center-block ending-image" src="' + imgURL + '" />');
+    setTimeout(function () {
+        player();
+    }, duration);
+}
+
+var player = function () {
+    $videoScreening.html('');
+    return new YT.Player('video-screening', {
+        height: '390',
+        width: '640',
+        videoId: playedVideo,
+        playerVars: {autoplay: 1, loop: 1, playlist: playedVideo},
+        events: {
+            'onReady': countDown
+        }
+    });
+};
 
 function performVideosSearch(query, token, callback) {
     var data = {
@@ -125,6 +157,9 @@ function countDown() {
             case 'Eat Snack':
                 imgURL = './img/snack-time.jpg';
                 break;
+            case 'Draw':
+                imgURL = './img/drawing.jpg';
+                break;
             default:
                 imgURL = './img/go-home.jpg';
                 break;
@@ -147,3 +182,14 @@ var searchResult = function(videoData, playId) {
               </div>\
             </div>';
 };
+
+/*jshint multistr: true */
+var prevideoActivityOptions =  '<input type="radio" name="prevideo-activity" id="previd-drawing" value="Draw" checked />\
+                                <label for="previd-drawing" class="radio-inline btn btn-default">Draw</label>\
+                                <input type="radio" name="prevideo-activity" id="previd-eat-snack" value="Eat Snack" />\
+                                <label for="previd-eat-snack" class="radio-inline btn btn-default">Eat Snack</label>\
+                                <br /><br />\
+                                <fieldset class="form-group">\
+                                <label for="prevideo-duration">Duration to do prevideo Activity</label>\
+                                <input type="number" name="prevideo-duration" class="form-control" placeholder="This will be in minutes, default to 2 mins..."/>\
+                                </fieldset>';
