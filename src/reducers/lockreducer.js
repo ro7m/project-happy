19c3ceee628eab6538522354e1fcf40be0be0657
ro7m <robin.mundra@gmail.com>
2024-10-27 16:31:25 +0530

    Create lockreducer.js

---
+++ src/reducers/lockreducer.js
@@ -0,0 +1,18 @@
+const initialLockState = {
+  isLocked: false
+};
+
+function lockReducer(state = initialLockState, action) {
+  switch (action.type) {
+    case 'LOCK_APP':
+      return {
+        isLocked: true
+      };
+    case 'UNLOCK_APP':
+      return {
+        isLocked: false
+      };
+    default:
+      return state;
+  }
+}
